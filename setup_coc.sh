2d9171618a577c79fa580950b177773b76755b55
Yuta Hayashibe <yuta@hayashibe.jp>
2021-11-01 11:04:29 +0900

    Updated

--- setup_coc.sh
+++ setup_coc.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 vim -c \
-    'CocInstall coc-markdownlint coc-diagnostic coc-css coc-htmlhint coc-eslint coc-json coc-yaml coc-texlab'
+    'CocInstall coc-markdownlint coc-diagnostic coc-css coc-htmlhint coc-eslint coc-json coc-yaml coc-texlab coc-pyright'
