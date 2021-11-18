27986ed04362e94415dcb66add6717aa423ede5e
Yuta Hayashibe <yuta@hayashibe.jp>
2021-11-18 12:32:54 +0900

    Use prettier for JS

--- setup_coc.sh
+++ setup_coc.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 vim -c \
-    'CocInstall coc-markdownlint coc-diagnostic coc-css coc-htmlhint coc-eslint coc-json coc-yaml coc-texlab coc-pyright coc-tsserver'
+    'CocInstall coc-markdownlint coc-diagnostic coc-css coc-htmlhint coc-eslint coc-json coc-yaml coc-texlab coc-pyright coc-tsserver coc-prettier'
