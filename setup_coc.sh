a229270bf3f7ead15706bd091386c963da344a96
Yuta Hayashibe <yuta@hayashibe.jp>
2022-04-18 18:41:47 +0900

    Add python setup

--- setup_coc.sh
+++ setup_coc.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+pip install -U isort black
 vim -c \
     'CocInstall coc-markdownlint coc-diagnostic coc-css coc-htmlhint coc-eslint coc-json coc-yaml coc-texlab coc-pyright coc-tsserver coc-prettier'
