ea178c06dbbfd796f130d297767a541e548c5be0
Yuta Hayashibe <yuta@hayashibe.jp>
2022-06-03 14:54:14 +0900

    Add

--- setup_coc.sh
+++ setup_coc.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 pip install -U isort black
+npm update --location=global markdownlint-cli esformatter eslint js-beautify
 vim -c \
     'CocInstall coc-markdownlint coc-diagnostic coc-css coc-htmlhint coc-eslint coc-json coc-yaml coc-texlab coc-pyright coc-tsserver coc-prettier'
