e61ac6805492d45fe710de6eed15f334f2fe6193
Yuta Hayashibe <yuta@hayashibe.jp>
2022-10-17 11:19:38 +0900

    Add

--- setup_coc.sh
+++ setup_coc.sh
@@ -5,6 +5,7 @@ ExistCmd() { echo "Checking $1" ; type "$1"; }
 ExistCmd pip || exit 1
 ExistCmd npm || exit 1
 
+vi +':call dein#update()' +q
 pip install -U isort black
 npm update --location=global markdownlint-cli esformatter eslint js-beautify
 vim -c \
