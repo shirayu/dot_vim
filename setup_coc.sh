ece9243b17795a1c9be871f37ff0610562c162c4
Yuta Hayashibe <yuta@hayashibe.jp>
2022-11-08 13:42:57 +0900

    Add dein lock (Shougo/dein.vim#353)

--- setup_coc.sh
+++ setup_coc.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
-ExistCmd() { echo "Checking $1" ; type "$1"; }
+ExistCmd() {
+    echo "Checking $1"
+    type "$1"
+}
 
 ExistCmd pip || exit 1
 ExistCmd npm || exit 1
 
-vi +':call dein#update()' +q
+if [[ $1 == "load" ]]; then
+    vi +':call dein#load_rollback(expand("~/.vim/dein.lock"))' +q
+else
+    vi +':call dein#update()' +q
+    vi +':call dein#save_rollback("$HOME/.vim/dein.lock")' +q
+fi
+
 pip install -U isort black
 npm update --location=global markdownlint-cli esformatter eslint js-beautify
 vim -c \
