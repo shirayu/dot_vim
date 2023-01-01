57de91cecb1ae0e3c56905065b1c800d62511417
Yuta Hayashibe <yuta@hayashibe.jp>
2023-01-01 16:27:55 +0900

    Updated

--- template/template.py
+++ template/template.py
@@ -4,7 +4,11 @@ import argparse
 from pathlib import Path
 
 
-def operation(path_in: Path, path_out: Path) -> None:
+def operation(
+    *,
+    path_in: Path,
+    path_out: Path,
+) -> None:
     with path_in.open() as inf, path_out.open("w") as outf:
         for line in inf:
             outf.write(line)
@@ -12,14 +16,21 @@ def operation(path_in: Path, path_out: Path) -> None:
 
 def get_opts() -> argparse.Namespace:
     oparser = argparse.ArgumentParser()
-    oparser.add_argument("--input", "-i", type=Path, default="/dev/stdin", required=False)
-    oparser.add_argument("--output", "-o", type=Path, default="/dev/stdout", required=False)
+    oparser.add_argument(
+        "--input", "-i", type=Path, default="/dev/stdin", required=False
+    )
+    oparser.add_argument(
+        "--output", "-o", type=Path, default="/dev/stdout", required=False
+    )
     return oparser.parse_args()
 
 
 def main() -> None:
     opts = get_opts()
-    operation(opts.input, opts.output)
+    operation(
+        path_in=opts.input,
+        path_out=opts.output,
+    )
 
 
 if __name__ == "__main__":
