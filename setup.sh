ecf342250383926e2610abfb69be9ccb90897d61
Yuta Hayashibe <yuta@hayashibe.jp>
2023-01-06 20:46:27 +0900

    Add pip lock

--- setup.sh
+++ setup.sh
@@ -11,18 +11,24 @@ ExistCmd python || exit 1
 ExistCmd vim || exit 1
 
 if [[ $1 == "load" ]]; then
-    vim +':call dein#load_rollback(expand("~/.vim/dein.lock.json"))' +q
+    vim +':call dein#load_rollback(expand("~/.vim/lock/dein.lock.json"))' +q
+
+    xargs -t pip install < "$HOME/.vim/locl/pip.lock" 
+
 elif [[ $1 == "update" ]]; then
     vim +':call dein#update()' +q
-    vim +':call dein#save_rollback("$HOME/.vim/dein.lock.json.tmp")' +q
-    python -m json.tool <"$HOME/.vim/dein.lock.json.tmp" >"$HOME/.vim/dein.lock.json"
-    rm "$HOME/.vim/dein.lock.json.tmp"
+    vim +':call dein#save_rollback("$HOME/.vim/lock/dein.lock.json.tmp")' +q
+    python -m json.tool <"$HOME/.vim/locl/dein.lock.json.tmp" >"$HOME/.vim/lock/dein.lock.json"
+    rm "$HOME/.vim/locl/dein.lock.json.tmp"
+
+    pip install -U isort black
+    pip list | grep -e 'isort' -e 'black' | sed -r 's/\s+/==/' > "$HOME/.vim/locl/pip.lock"
+
 else
     echo "Usage: $0 [load|update]" >&2
     exit 1
 fi
 
-pip install -U isort black
 npm update --location=global markdownlint-cli esformatter eslint js-beautify
 vim -c \
     'CocInstall coc-markdownlint coc-diagnostic coc-css coc-htmlhint coc-eslint coc-json coc-yaml coc-texlab coc-pyright coc-tsserver coc-prettier'
