2020e4c7466fbbfa1e08286cd24d3e7e57ffab43
Yuta Hayashibe <yuta@hayashibe.jp>
2024-11-12 22:07:44 +0900

    Updated template.sh

--- template/template.sh
+++ template/template.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-echo ${SCRIPT_DIR}
+SCRIPT_DIR=$(
+    cd "$(dirname "${BASH_SOURCE:-$0}")" || exit 1 \
+        && pwd
+)
+echo "${SCRIPT_DIR}"
 
 TMPDIR=$(mktemp -d)
 trap 'rm -rf "${TMPDIR}"' EXIT INT TERM HUP
