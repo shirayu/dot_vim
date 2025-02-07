576a445f1f47f157012bcf55a287f9f983b9b4b9
Yuta Hayashibe <yuta@hayashibe.jp>
2025-02-07 12:38:21 +0900

    Use global ruff

--- setup.sh
+++ setup.sh
@@ -11,13 +11,6 @@ ExistCmd pip || exit 1
 ExistCmd python || exit 1
 ExistCmd vim || exit 1
 
-PATH_COC_RUFF_DIR=~/.config/coc/extensions/@yaegassy/coc-ruff-data/ruff-lsp
-PATH_COC_RUFF_VENV=${PATH_COC_RUFF_DIR}/venv
-if [[ -e $PATH_COC_RUFF_VENV && ! -L $PATH_COC_RUFF_VENV ]]; then
-    echo "Error: $PATH_COC_RUFF_VENV exists and is not a symbolic link." >&2
-    exit 1
-fi
-
 if [[ $1 == "load" ]]; then
     vim +':call dein#load_rollback(expand("~/.vim/lock/dein.lock.json"))' +q
 
@@ -67,28 +60,3 @@ else
         npm list --json >"$HOME/.vim/lock/coc.package.lock.json"
     )
 fi
-
-(
-    DIR_VENV=~/.vim/tools/venv
-    VENV_PIP="${DIR_VENV}/bin/pip"
-    VENV_LOCK_FILE="$HOME/.vim/lock/coc_ruff.pip.lock.json"
-
-    if [[ ! -e ${DIR_VENV} ]]; then
-        python -m venv "${DIR_VENV}"
-        "${VENV_PIP}" install ruff-lsp
-    fi
-
-    if [[ ! -e $PATH_COC_RUFF_VENV ]]; then
-        mkdir -p ${PATH_COC_RUFF_DIR}
-        ln -s "${DIR_VENV}" "${PATH_COC_RUFF_DIR}"
-    fi
-
-    if [[ $1 == "load" ]]; then
-        python -c 'import json,sys; print(" ".join([f"{x["name"]}=={x["version"]}" for x in json.load(sys.stdin)]))' \
-            <"${VENV_LOCK_FILE}" \
-            | xargs -t ~/.vim/tools/venv/bin/pip install
-    else
-        "${VENV_PIP}" list --format freeze | cut -f1 -d= | xargs "${VENV_PIP}" install -U
-        "${VENV_PIP}" list --format json | "${DIR_VENV}/bin/python" -c "import sys,json;print(json.dumps(json.loads(sys.stdin.read()),indent=2,ensure_ascii=False,sort_keys=True))" >"${VENV_LOCK_FILE}"
-    fi
-)
